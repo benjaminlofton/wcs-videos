bd607c5b62ab196fe26af5ac73683535e2b086c5
Blake Norrish <blakenorrish@livenation.com>
2015-07-14 19:20:27 -0700

    Removed console output of loaded beans on start-up

--- videos-backend/src/main/java/ban/Application.java
+++ videos-backend/src/main/java/ban/Application.java
@@ -17,15 +17,7 @@ public class Application extends SpringBootServletInitializer {
   }
 
   public static void main(String[] args) {
-    ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-    System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-    String[] beanNames = ctx.getBeanDefinitionNames();
-    Arrays.sort(beanNames);
-    for (String beanName : beanNames) {
-      System.out.println(beanName);
-    }
+    SpringApplication.run(Application.class, args);
   }
 }
 
