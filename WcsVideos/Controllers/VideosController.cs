bb766793769cc0083e9481116d57bba01aa45a4d
benjaminlofton <benjaminlofton@users.noreply.github.com>
2015-07-18 00:42:42 -0400

    Add dancer search in AddVideo page

--- WcsVideos/Controllers/VideosController.cs
+++ WcsVideos/Controllers/VideosController.cs
@@ -11,6 +11,8 @@ namespace WcsVideos.Controllers
 {
     public class VideosController : Controller
     {
+        private const int ResultsPerPage = 20;
+        
         private IDataAccess dataAccess;
         
         public VideosController(IDataAccess dataAccess)
@@ -134,6 +136,61 @@ namespace WcsVideos.Controllers
                 new { controller = "Videos", action = "Add" });
 
             return this.View(model);
+        }
+        
+        public IActionResult DancerSearchResults(string query, int start)
+        {
+            DancerSearchResultsViewModel viewModel = new DancerSearchResultsViewModel();
+            List<DancerListItemViewModel> results = new List<DancerListItemViewModel>();
+            viewModel.Query = query;
+            viewModel.Entries = results;
+            
+            if (start <= 0)
+            {
+                start = 1;
+            }
+            
+            if (!string.IsNullOrEmpty(query))
+            {
+                viewModel.ShowResults = true;
+                
+                List<Dancer> fullResults = this.dataAccess.SearchForDancer(query);
+                viewModel.ResultsTotal = fullResults.Count;
+                
+                foreach(Dancer dancer in fullResults.Skip(start - 1).Take(VideosController.ResultsPerPage))
+                {
+                    DancerListItemViewModel listItem = new DancerListItemViewModel();
+                    listItem.Name = dancer.Name;
+                    listItem.VideoCount = dancer.VideoIdList == null ? 0 : dancer.VideoIdList.Length;
+                    listItem.WsdcId = dancer.WsdcId;
+                    listItem.Url = string.Format("javascript:addDancer('{0}');", dancer.WsdcId);
+                    results.Add(listItem);
+                }
+                
+                viewModel.ResultsStart = start;
+                viewModel.ResultsEnd = start + results.Count - 1;
+                viewModel.ResultsTotal = fullResults.Count;
+                
+                if (viewModel.ResultsEnd < fullResults.Count)
+                {
+                    viewModel.ShowNextLink = true;
+                    viewModel.NextLinkUrl = string.Format(
+                        "javascript:searchForDancer('{0}', {1});",
+                        query,
+                        viewModel.ResultsEnd + 1);
+                }
+                
+                if (start > 1)
+                {
+                    viewModel.ShowPreviousLink = true;
+                    viewModel.PreviousLinkUrl = string.Format(
+                        "javascript:searchForDancer('{0}', {1});",
+                        query,
+                        viewModel.ResultsStart - VideosController.ResultsPerPage);
+                }
+            }
+            
+            return View(viewModel);
         }
         
         public IActionResult Error()
