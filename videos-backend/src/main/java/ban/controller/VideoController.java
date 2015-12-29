f25386cf1c7aea95bdc9a5c665e4d096cbfca291
Blake Norrish <blakenorrish@livenation.com>
2015-12-29 13:35:45 -0800

    Added DanceCategory support to /v? endpoint

--- videos-backend/src/main/java/ban/controller/VideoController.java
+++ videos-backend/src/main/java/ban/controller/VideoController.java
@@ -60,11 +60,12 @@ public class VideoController {
   public ResponseEntity<List<Video>> searchVideos(
       @RequestParam(value = "wsdc-id", required = false) String wsdcIdList,
       @RequestParam(value = "level", required = false) String level,
+      @RequestParam(value = "category", required = false) String category,
       @RequestParam(value = "title-frag", required =  false) String titleFragList,
       @RequestParam(value = "provider-id", required = false) String providerVideoId,
       @RequestParam(value = "event-id", required = false) String eventIdList) {
 
-    if(wsdcIdList == null && titleFragList == null && providerVideoId == null && eventIdList == null && level == null) {
+    if(wsdcIdList == null && titleFragList == null && providerVideoId == null && eventIdList == null && level == null && category == null) {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
@@ -82,7 +83,7 @@ public class VideoController {
       return new ResponseEntity<>(Arrays.asList(v),HttpStatus.OK);
     }
 
-    List<Video> videos = videoSearchService.search(wsdcIdList,titleFragList,eventIdList, level);
+    List<Video> videos = videoSearchService.search(wsdcIdList,titleFragList,eventIdList,level, category);
     return new ResponseEntity<>(videos,HttpStatus.OK);
   }
 
--- videos-backend/src/main/java/ban/service/VideoSearchService.java
+++ videos-backend/src/main/java/ban/service/VideoSearchService.java
@@ -4,10 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import ban.client.AwsDynamoClient;
+import ban.model.persistence.DanceCategory;
+import ban.model.persistence.DancerD;
 import ban.model.persistence.EventD;
 import ban.model.persistence.SkillLevel;
 import ban.model.persistence.VideoD;
@@ -27,6 +32,9 @@ public class VideoSearchService {
   @Autowired
   VideoMapper videoMapper;
 
+  @Autowired
+  AwsDynamoClient awsDynamoClient;
+
   /**
    * Get list of Videos by provided search parameters
    * If any parameter is null, it is ignored.
@@ -36,17 +44,20 @@ public class VideoSearchService {
    * @param titleFragList - comma separated list of title words
    * @param eventIdList - comma separated list of event Ids
    * @param strLevel - string representation of SkillLevel
+   * @param strCategory - string representation of DanceCategory
    * @return List of video objects matching all non-null search parameters
    */
-  public List<Video> search(String wsdcIdList, String titleFragList, String eventIdList, String strLevel) {
+  public List<Video> search(String wsdcIdList, String titleFragList, String eventIdList, String strLevel, String strCategory) {
 
-    // This should / will throw exception if strLevel cannot be mapped to SkillLevel Enum
+    // This should / will throw exception if strings cannot be mapped to Enum values
     SkillLevel level = strLevel == null ? null : SkillLevel.valueOf(strLevel);
+    DanceCategory category = strCategory == null ? null : DanceCategory.valueOf(strCategory);
 
     List<VideoD> videos = localIndexedDataService.getAllVideos();
 
     return videos.stream()
                  .filter(keepWithLevel(level))
+                 .filter(keepWithCategory(category))
                  .filter(keepWithAnyWsdc(wsdcIdList))
                  .filter(keepWithAnyTitleFrag(titleFragList))
                  .filter(keepWithAnyEventId(eventIdList))
@@ -65,6 +76,20 @@ public class VideoSearchService {
       }
 
       return d.getSkillLevel().equals(level);
+    };
+  }
+
+  public static Predicate<VideoD> keepWithCategory(DanceCategory category) {
+    return d -> {
+      if(category == null) {
+        return true;
+      }
+
+      if(d.getDanceCategory() == null) {
+        return false;
+      }
+
+      return d.getDanceCategory().equals(category);
     };
   }
 
