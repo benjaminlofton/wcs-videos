de2ab04c2cf1aeb1f91a1d3d3f442ec2e7535e6a
benjaminlofton <benjaminlofton@users.noreply.github.com>
2016-01-07 18:45:49 -0800

    Add stats inline in admin console

--- WcsVideos/Models/AdminIndexViewModel.cs
+++ WcsVideos/Models/AdminIndexViewModel.cs
@@ -4,10 +4,28 @@ namespace WcsVideos.Models
 	{
         public string MissingEventVideoListUrl { get; set; }
         
+        public int MissingEventCount { get; set; }
+        
         public string MissingDancersVideoListUrl { get; set; }
+        
+        public int MissingDancersCount { get; set; }
         
         public string MissingLevelVideoListUrl { get; set; }
         
+        public int MissingLevelCount { get; set; }
+        
         public string MissingCategoryVideoListUrl { get; set; }
+        
+        public int MissingCategoryCount { get; set; }
+        
+        public int EventCount { get; set; }
+        
+        public int DancerCount { get; set; }
+        
+        public int VideoCount { get; set; }
+        
+        public int SuggestedVideoCount { get; set; }
+        
+        public int FlaggedVideoCount { get; set; }
     }
 }
