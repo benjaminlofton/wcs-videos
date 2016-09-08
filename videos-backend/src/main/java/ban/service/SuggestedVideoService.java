2681dbe717609f865ef5a25fbd0847d9fcf5b81a
Blake Norrish <blakenorrish@livenation.com>
2016-09-08 11:23:59 -0700

    Suggested videos are returned most-recently-submitted-first

--- videos-backend/src/main/java/ban/service/SuggestedVideoService.java
+++ videos-backend/src/main/java/ban/service/SuggestedVideoService.java
@@ -1,9 +1,15 @@
 package ban.service;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import ban.client.AwsDynamoClient;
 import ban.model.persistence.VideoD;
@@ -24,6 +30,10 @@ public class SuggestedVideoService {
 
   public Video recordSuggestedVideo(Video video) {
 
+    // Set CreatedDateTime
+    DateTimeFormatter dtf = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+    video.setCreatedDateTime(dtf.print(new DateTime(DateTimeZone.UTC)));
+
     VideoD videoD = videoMapper.mapToPersistanceModel(video);
 
     videoD = awsDynamoClient.recordSuggestedVideo(videoD);
@@ -35,7 +45,11 @@ public class SuggestedVideoService {
 
     List<VideoD> videos = awsDynamoClient.getAllVideos("SuggestedVideo");
 
-    return videoMapper.mapToViewModel(videos);
+    // Order by CreateDateTime, map to ViewModel
+    return videos.stream()
+          .sorted(byCreatedDateTime)
+          .map(videoMapper::mapToViewModel)
+          .collect(Collectors.toList());
   }
 
   public void deleteVideo(String videoId) {
@@ -43,4 +57,16 @@ public class SuggestedVideoService {
     awsDynamoClient.deleteSuggestedVideo(videoId);
 
   }
+
+  private static final Comparator<VideoD> byCreatedDateTime = (v1, v2) -> {
+    if (v1.getCreatedDateTime() == null) {
+      return 1;
+    }
+
+    if (v2.getCreatedDateTime() == null) {
+      return -1;
+    }
+
+    return v2.getCreatedDateTime().compareTo(v1.getCreatedDateTime());
+  };
 }
